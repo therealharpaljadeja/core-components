eabf908b19178c6f70af4452e697ab1eff23b025
Harpal Jadeja <jadejaharpal14@gmail.com>
2022-09-24 22:54:38 +0530

    feat: add input playground and stories

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -1,19 +1,61 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Input from "./Input";
-import { AiFillYoutube } from "react-icons/ai";
+import { AiOutlineMail } from "react-icons/ai";
+import { TypePredicateKind } from "typescript";
 
 export default {
-	title: "Input",
-	component: Input,
+    title: "Input",
+    component: Input,
 } as ComponentMeta<typeof Input>;
 
 const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
 
-export const ReusableInput = Template.bind({});
-ReusableInput.args = {
-	label: "Email",
-	disabled: true,
-	leftIcon: <AiFillYoutube />,
-	error: true,
-	errorMessage: "This is a error message.",
+export const Default = Template.bind({});
+
+export const withLabel = Template.bind({});
+withLabel.args = {
+    ...Default.args,
+    label: "Email",
+};
+
+export const withPrefix = Template.bind({});
+withPrefix.args = {
+    ...Default.args,
+    prefix: "https://",
+    hint: "Enter URL",
+};
+
+export const withLeftIcon = Template.bind({});
+withLeftIcon.args = {
+    ...withLabel.args,
+    leftIcon: <AiOutlineMail />,
+};
+
+export const withHint = Template.bind({});
+withHint.args = {
+    ...withLeftIcon.args,
+    hint: "Enter your email address for eg:- hey@gmail.com",
+};
+
+export const withErrorMessage = Template.bind({});
+withErrorMessage.args = {
+    ...withHint.args,
+    error: true,
+    errorMessage: "Invalid Email Address",
+};
+
+export const disabled = Template.bind({});
+disabled.args = {
+    ...withLabel.args,
+    disabled: true,
+};
+
+export const Playground = Template.bind({});
+Playground.args = {
+    label: "Email",
+    disabled: true,
+    leftIcon: <AiOutlineMail />,
+    error: false,
+    hint: "This is a hint",
+    errorMessage: "This is a error message.",
 };
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,67 +3,84 @@ import clsx from "clsx";
 import { MdOutlineErrorOutline } from "react-icons/md";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-	label?: string;
-	className?: string;
-	leftIcon?: ReactNode;
-	hint?: string;
-	errorMessage?: string;
-	error?: boolean;
+    label?: string;
+    className?: string;
+    leftIcon?: ReactNode;
+    hint?: string;
+    errorMessage?: string;
+    error?: boolean;
+    prefix?: string;
 }
 
 const Input = ({
-	label,
-	className,
-	leftIcon,
-	placeholder = "placeholder",
-	type = "text",
-	...props
+    label,
+    className,
+    leftIcon,
+    placeholder = "placeholder",
+    type = "text",
+    prefix,
+    ...props
 }: InputProps): React.ReactElement => {
-	return (
-		<label className='w-full'>
-			{label && (
-				<div className='text-gray-700 mb-[6px] font-display'>
-					{label}
-				</div>
-			)}
-			<div className='flex items-center'>
-				<input
-					className={clsx(
-						{
-							"pl-[45px]": leftIcon,
-							"focus:ring-error-200 focus:!border-error-300 border-error-300 pr-[45px]":
-								props.error,
-						},
-						"w-full",
-						"font-display",
-						"px-[14px] py-[10px]",
-						"border-2 border-gray-300 rounded-lg",
-						"focus:border-primary-300",
-						"focus:ring-2 focus:ring-offset-0 focus:ring-primary-200",
-						"disabled:bg-gray-50",
-						"outline-none"
-					)}
-					type={type}
-					placeholder={placeholder}
-					{...props}
-				/>
-				<span className='absolute h-5 w-5 flex items-center left-[35px]'>
-					{leftIcon}
-				</span>
-				{props.error && (
-					<MdOutlineErrorOutline className='absolute h-5 w-5 right-[35px] text-error-500' />
-				)}
-			</div>
-			{props.hint && (
-				<div className='text-gray-500 font-display'>{props.hint}</div>
-			)}
-			{props.error && props.errorMessage && (
-				<div className='text-error-500 font-display'>
-					{props.errorMessage}
-				</div>
-			)}
-		</label>
-	);
+    return (
+        <label className="w-full">
+            {label && (
+                <div className="text-gray-700 mb-[6px] font-inter">{label}</div>
+            )}
+            <div className="flex w-full">
+                {prefix && (
+                    <span className="rounded-l-lg bg-gray-300 inline-flex items-center px-3 font-inter text-gray-600">
+                        {prefix}
+                    </span>
+                )}
+                <div
+                    className={clsx(
+                        {
+                            "!bg-gray-50": props.disabled,
+                            "rounded-r-lg": prefix,
+                            "rounded-lg": !prefix,
+                        },
+                        {
+                            "focus-within:ring-error-200 focus-within:!border-error-300 border-error-300":
+                                props.error,
+                        },
+                        "flex w-full bg-base-white px-[14px] py-[10px] focus-within:ring-2 focus-within:ring-offset-0 focus-within:ring-primary-200 font-inter items-center border-2 border-gray-300  focus-within:border-primary-300"
+                    )}
+                >
+                    <input
+                        className={clsx("peer w-full", "outline-none", {
+                            "!bg-gray-50": props.disabled,
+                        })}
+                        type={type}
+                        placeholder={placeholder}
+                        {...props}
+                    />
+                    {leftIcon && (
+                        <span
+                            className={clsx(
+                                { "!text-error-400": props.error },
+                                "peer-focus:text-primary-400 order-first mr-2 h-5 w-5 flex items-center"
+                            )}
+                        >
+                            {leftIcon}
+                        </span>
+                    )}
+                    {props.error && (
+                        <MdOutlineErrorOutline className="h-5 w-5 text-error-500" />
+                    )}
+                </div>
+            </div>
+            <div className="mt-[6px]">
+                {props.hint && !props.error && (
+                    <div className="text-gray-500 font-inter">{props.hint}</div>
+                )}
+                {props.error && props.errorMessage && (
+                    <div className="text-error-500 font-inter">
+                        {props.errorMessage}
+                    </div>
+                )}
+            </div>
+        </label>
+    );
 };
 
 export default Input;
