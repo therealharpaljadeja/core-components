62b75c71397f85997d7aef3a9a2bc6f1ebb1ec96
Harpal Jadeja <jadejaharpal14@gmail.com>
2022-09-24 18:33:33 +0530

    feat: add badge playground, withIcon, invertedColorScheme and variants

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,20 @@
 import "../src/index.css";
 
 export const parameters = {
-	actions: { argTypesRegex: "^on[A-Z].*" },
-	controls: {
-		matchers: {
-			color: /(background|color)$/i,
-			date: /Date$/,
-		},
-	},
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    backgrounds: {
+        default: "default",
+        values: [
+            {
+                name: "default",
+                value: "#F4F5F7",
+            },
+        ],
+    },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
+    },
 };
--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -1,24 +1,74 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import Badge from "./Badge";
-import { AiOutlineRight } from "react-icons/ai";
+import Badge, { BadgeSize, BadgeVariant } from "./Badge";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 
 export default {
-	title: "Badge",
-	component: Badge,
+    title: "Components/Badge",
+    component: Badge,
+    argTypes: {
+        invertColorScheme: {
+            type: "boolean",
+        },
+    },
 } as ComponentMeta<typeof Badge>;
 
 const Template: ComponentStory<typeof Badge> = (args) => <Badge {...args} />;
-
-export const SimpleBadge = Template.bind({});
-SimpleBadge.args = {};
-
-export const BadgeWithIcon = Template.bind({});
-BadgeWithIcon.args = {
-	element: <AiOutlineRight />,
+export const Playground = Template.bind({});
+Playground.args = {
+    label: "Badge",
+    size: BadgeSize.md,
+    variant: BadgeVariant.Primary,
+    invertColorScheme: false,
 };
 
-export const BadgeWithRightIcon = Template.bind({});
-BadgeWithRightIcon.args = {
-	element: <AiOutlineRight />,
-	left: false,
-};
+export const Variants = () => (
+    <div className="flex items-center space-x-4">
+        <Badge label="Badge" variant={BadgeVariant.Primary} />
+        <Badge label="Badge" variant={BadgeVariant.Tertiary} />
+        <Badge label="Badge" variant={BadgeVariant.Success} />
+        <Badge label="Badge" variant={BadgeVariant.Error} />
+        <Badge label="Badge" variant={BadgeVariant.Warning} />
+    </div>
+);
+
+export const withIcon = () => (
+    <div className="flex space-x-4">
+        <Badge leftIcon={<AiOutlineArrowLeft />} label="Badge" />
+        <Badge rightIcon={<AiOutlineArrowRight />} label="Badge" />
+        <Badge
+            leftIcon={<AiOutlineArrowLeft />}
+            rightIcon={<AiOutlineArrowRight />}
+            label="Badge"
+        />
+    </div>
+);
+
+export const withInvertedColorScheme = () => (
+    <div className="flex items-center space-x-4">
+        <Badge
+            label="Badge"
+            variant={BadgeVariant.Primary}
+            invertColorScheme={false}
+        />
+        <Badge
+            label="Badge"
+            variant={BadgeVariant.Tertiary}
+            invertColorScheme={false}
+        />
+        <Badge
+            label="Badge"
+            variant={BadgeVariant.Success}
+            invertColorScheme={false}
+        />
+        <Badge
+            label="Badge"
+            variant={BadgeVariant.Error}
+            invertColorScheme={false}
+        />
+        <Badge
+            label="Badge"
+            variant={BadgeVariant.Warning}
+            invertColorScheme={false}
+        />
+    </div>
+);
